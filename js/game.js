ea6d04c90dfe29f3da9d7037d36979d4115c44d3
janet-do <124389477+janet-do@users.noreply.github.com>
2023-04-14 16:43:47 +0000

    sync

---
+++ js/game.js
@@ -0,0 +1,13 @@
+// Copyright (c) 2020 Janet Do All rights reserved
+//
+// Created by: Janet Do
+// Created on: Sep 2020
+/* global Phaser */
+const config = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  backgroundColor: 0x5f6e7a,
+}
+const game = new Phaser.Game(config)
+console.log(game)
