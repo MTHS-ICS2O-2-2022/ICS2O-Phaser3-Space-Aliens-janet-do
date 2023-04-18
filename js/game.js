590b74fe209034600c6df427165b0456fd0350f2
janet-do <124389477+janet-do@users.noreply.github.com>
2023-04-18 16:58:41 +0000

    sync

--- js/game.js
+++ js/game.js
@@ -7,7 +7,18 @@ const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: true,
+    },
+  },
+  //set background color
   backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
 }
 const game = new Phaser.Game(config)
 console.log(game)
