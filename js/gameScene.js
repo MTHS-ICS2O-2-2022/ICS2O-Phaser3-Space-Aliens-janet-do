9ff6a33422d5071f45950013500263fe1ae9e312
janet-do <124389477+janet-do@users.noreply.github.com>
2023-05-05 17:05:54 +0000

    sync

--- js/gameScene.js
+++ js/gameScene.js
@@ -14,6 +14,9 @@ class GameScene extends Phaser.Scene {
    */
   constructor() {
     super({ key: "gameScene" })
+
+    this.background = null
+    this.ship = null
   }
   /**
    * Can be defined on your own Scenes
@@ -31,6 +34,9 @@ class GameScene extends Phaser.Scene {
    */
   preload() {
     console.log("Game Scene")
+
+    this.load.image("starBackground", "./assets/starBackground.png")
+    this.load.image("ship", "./assets/spaceShip.png")
   }
   /**
    * Can be defined on your own Scenes
@@ -38,7 +44,10 @@ class GameScene extends Phaser.Scene {
    * @param {object} data - any data passed via  ScenePlugin.add() or ScenePlugin.start()
    */
   create(data) {
-    //pass
+    this.background = this.add.sprite(0, 0, "starBackground").setScale(2.0)
+    this.background.setOrigin(0, 0)
+
+    this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship")
   }
 
   /**
